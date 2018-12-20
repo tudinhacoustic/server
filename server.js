7a39fd92e914790ef25c2e39e81988f1a9d67238
tuacoustic <tudncps05984@fpt.edu.vn>
2018-12-20 21:25:43 +0700

    make it better

--- server.js
+++ server.js
@@ -10,6 +10,19 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 
+//CORS
+app.use((req, res, next)=>{
+	res.render("Access-Control-Allow-Origin","*");
+	res.header("Access-Control-Allow-Headers",
+	"Origin, X-Requested-With, Content-Type, Accept, Authorization");
+	if(req.method === "OPTIONS"){
+		res.header("Access-Control-Allow-Methods", 
+		"PUT, POST, PATCH, DELETE, GET");
+		return res.status(200).json({});
+	}
+})
+
+
 const db = require('./config/keys').mongoURI;
 
 mongoose
