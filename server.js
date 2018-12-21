3f04eb3fcbbad45db02b65e047fc98ff89098a48
tuacoustic <tudncps05984@fpt.edu.vn>
2018-12-21 09:15:28 +0700

    make it better

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 //CORS
 app.all('*', function(req, res, next) {
      var origin = req.get('origin');
-     res.header('Access-Control-Allow-Origin', 'http://tu-api.herokuapp.com');
+     res.header('Access-Control-Allow-Origin', '*');
     //  res.header("Access-Control-Allow-Headers", "X-Requested-With");
      res.header('Access-Control-Allow-Headers', 'Content-Type');
      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
